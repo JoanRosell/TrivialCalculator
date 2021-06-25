cfa3c69f009142c80bcd5c69bc412d8664209596
JoanRosell <joan.rosellr@gmail.com>
2021-06-25 13:46:57 +0200

    feat: implement foreach loop. closes #1
    Signed-off-by: JoanRosell <joan.rosellr@gmail.com>

---
+++ MyFirstApp/ForEachLoop/frmForEach.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFirstApp.ForEachLoop
+{
+    public partial class frmForEach : Form
+    {
+        public frmForEach()
+        {
+            InitializeComponent();
+        }
+
+        private void txtStudentId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var newStud = new Student();
+            newStud.StudentId = Convert.ToInt32(txtStudentId.Text);
+            newStud.Name = txtName.Text;
+            newStud.Surname = txtSurname.Text;
+            newStud.Age = Convert.ToInt32(txtStudentId.Text);
+            var studentList = new ArrayList();
+            studentList.Add(newStud);
+
+            foreach (Student student in studentList)
+            {
+                MessageBox.Show($"{student.Name} {student.Surname}");
+            }
+            
+        }
+
+        private void txtSurname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
