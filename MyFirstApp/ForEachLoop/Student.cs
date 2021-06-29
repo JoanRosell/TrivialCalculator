ef42a2ca4c9de2d64540ff59d98791e241ab0602
JoanRosell <joan.rosellr@gmail.com>
2021-06-29 16:41:54 +0200

    feat: add while loop, refs #5

--- MyFirstApp/ForEachLoop/Student.cs
+++ MyFirstApp/ForEachLoop/Student.cs
@@ -12,5 +12,24 @@ namespace MyFirstApp.ForEachLoop
         public String Name { get; set; }
         public String Surname { get; set; }
         public int Age { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Student student &&
+                   StudentId == student.StudentId &&
+                   Name == student.Name &&
+                   Surname == student.Surname &&
+                   Age == student.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1235398547;
+            hashCode = hashCode * -1521134295 + StudentId.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Surname);
+            hashCode = hashCode * -1521134295 + Age.GetHashCode();
+            return hashCode;
+        }
     }
 }
